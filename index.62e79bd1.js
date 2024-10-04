8e23f93c896f93ad6cb48665698d776c3a6737c7
Oleksii Karpov <alneon1@gmail.com>
2024-10-04 10:51:43 +0300

    make build

---
+++ index.62e79bd1.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=(e.clientWidth-t.offsetWidth)/2,i=(e.clientHeight-t.offsetHeight)/2;t.style.top=i+"px",t.style.left=l+"px";
+//# sourceMappingURL=index.62e79bd1.js.map
